39f229caf03d2d474feb23e4076cd480fa9cc816
ShadowPeo <ShadowPeo@users.noreply.github.com>
2024-05-08 13:55:19 +1000

    Changes to be committed: 	modified:   README.md 	modified:   certificateManagement.ps1

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # ps_Papercut-LE-Certificates
 Powershell script to automatically pull a certificate from Lets-Encrypt and install it into Papercut (MF/NG) and Papercut Mobility Print servers
 
-In Addition to the licence conditions spelled out in the attached licence, the Victorian Department of Education is prohibited from utilising this resource until they treat their technicians like people and not slaves and are prohibited from using it in any way to support SCL or TID initiatives, permanently for the former, and until there is full public disclosure including the PIA to all staff with no reservations or NDA's on the later, this is not to say that individual schools cannot use the resource, they are most welcome to, DET themselves, however, cannot.
+This utilises and requires the Posh-ACME module, and is configured to use cloudflare DNS for the challenge
+
+In Addition to the licence conditions spelled out in the attached licence, the Victorian Department of Education is prohibited from utilising this resource in any way to support SCL or SID initiatives, permanently for the former, and until there is full public disclosure including the PIA to all staff with no reservations or NDA's on the later, this is not to say that individual schools cannot use the resource, they are most welcome to, DET themselves, however, cannot.
--- certificateManagement.ps1
+++ certificateManagement.ps1
@@ -22,6 +22,7 @@ $retrievedCert = Get-PACertificate -MainDomain $certNames[0]
 if ($null -eq $retrievedCert)
 {
     #Request certificate if ite does not exist
+    Write-Host "No Certificate Fount Retrieving new Certificate"
     New-PACertificate $certNames -AcceptTOS -Plugin Cloudflare -PluginArgs $pArgs
     
     #Pull newly retrieved certificate
@@ -30,16 +31,33 @@ if ($null -eq $retrievedCert)
 elseif (((New-TimeSpan -Start (Get-Date) -End ($retrievedCert.NotAfter)) -le $renewDays))
 {
     #Renew cert - add to else if it does exist try renewal
+    Write-Host "Certificate due for renewal, ignoring"
     Submit-Renewal -MainDomain $certNames[0]
+}
+else
+{
+    Write-Host "Certificate is current, continuing"
 }
 
 $mobilityPrintCert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 "C:\Program Files (x86)\PaperCut Mobility Print\data\tls.cer"
 
 if ($mobilityPrintCert.NotBefore -lt $retrievedCert.NotBefore)
 {
+    Write-Host "Certificate in store newer than one in Mobility Print, proceeding with updating the certificate"
     Copy-Item -Path $retrievedCert.FullChainFile -Destination "C:\Program Files (x86)\PaperCut Mobility Print\data\tls.cer" -Force
     Copy-Item -Path $retrievedCert.KeyFile -Destination "C:\Program Files (x86)\PaperCut Mobility Print\data\tls.pem" -Force
     Restart-Service -Name pc-mobility-print -Force
     Restart-Service -Name pc-print-deploy -Force
+    Write-Host "Updating the Papercut Keystore"
+    Start-Process 'C:\Program Files\PaperCut MF\server\bin\win\create-ssl-keystore.exe' -ArgumentList " -f -k `"C:\Program Files\PaperCut MF\server\custom\my-ssl-keystore`" -cert $($retrievedCert.FullChainFile) -key $($retrievedCert.KeyFile) -keystoreentry standard" -WorkingDirectory "C:\Program Files\PaperCut MF\server\bin\win\" -Wait -NoNewWindow
+    Restart-Service -Name PCAppServer -Force
+}
+elseif($retrievedCert.NotBefore -lt $mobilityPrintCert.NotBefore)
+{
+    Write-Host "Certificate in Mobility Print folder is newer than the one in the store, ingnoring one in store"
+}
+elseif($retrievedCert.Thumbprint -eq $mobilityPrintCert.Thumbprint)
+{
+    Write-Host "Certificate in Mobility Print folder matches the one in the store, ignoring"
 }
 
